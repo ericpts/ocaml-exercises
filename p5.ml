2bd685e5a685217273a2e187f03a087e65a3daa0
Eric P. Stavarache <ericptst@gmail.com>
2016-07-04 23:41:07 +0300

    Improved p5 efficiency

--- p5.ml
+++ p5.ml
@@ -1,9 +1,9 @@
 
-let rev (ls: 'a list):  ('a list) = 
-  let rec _rev (remaining: 'a list) (current: 'a list):  ('a list) = 
+let rev ls = 
+  let rec _rev remaining current = 
     match remaining with
     | [] -> current
-    | hd::tl -> _rev tl current@[hd]
+    | hd::tl -> _rev tl (hd::current)
   in
   _rev ls []
 ;;
